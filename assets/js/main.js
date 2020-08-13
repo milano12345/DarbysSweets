33c73d3d832ca2e83af301d33223f8f1d7abf307
milano12345 <dmilano857@gmail.com>
2020-08-13 15:03:38 -0600

    finished building darbys sweets structure

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,64 +4,78 @@
 	License: pixelarity.com/license
 */
 
-(function($) {
+(function ($) {
+  var $window = $(window),
+    $body = $("body");
 
-	var	$window = $(window),
-		$body = $('body');
+  // Breakpoints.
+  breakpoints({
+    wide: ["1281px", "1680px"],
+    normal: ["981px", "1280px"],
+    narrow: ["841px", "980px"],
+    narrower: ["737px", "840px"],
+    mobile: ["481px", "736px"],
+    mobilep: [null, "480px"],
+  });
 
-	// Breakpoints.
-		breakpoints({
-			wide:      [ '1281px',  '1680px' ],
-			normal:    [ '981px',   '1280px' ],
-			narrow:    [ '841px',   '980px'  ],
-			narrower:  [ '737px',   '840px'  ],
-			mobile:    [ '481px',   '736px'  ],
-			mobilep:   [ null,      '480px'  ]
-		});
+  // Play initial animations on page load.
+  $window.on("load", function () {
+    window.setTimeout(function () {
+      $body.removeClass("is-preload");
+    }, 100);
+  });
 
-	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
+  // Dropdowns.
+  $("#nav > ul").dropotron({
+    offsetY: -15,
+    hoverDelay: 0,
+    alignment: "center",
+  });
 
-	// Dropdowns.
-		$('#nav > ul').dropotron({
-			offsetY: -15,
-			hoverDelay: 0,
-			alignment: 'center'
-		});
+  // Nav.
 
-	// Nav.
+  // Bar.
+  $(
+    '<div id="titleBar">' +
+      '<a href="#navPanel" class="toggle"></a>' +
+      '<span class="title">' +
+      $("#logo").html() +
+      "</span>" +
+      "</div>"
+  ).appendTo($body);
 
-		// Bar.
-			$(
-				'<div id="titleBar">' +
-					'<a href="#navPanel" class="toggle"></a>' +
-					'<span class="title">' + $('#logo').html() + '</span>' +
-				'</div>'
-			)
-				.appendTo($body);
-
-		// Panel.
-			$(
-				'<div id="navPanel">' +
-					'<nav>' +
-						$('#nav').navList() +
-					'</nav>' +
-				'</div>'
-			)
-				.appendTo($body)
-				.panel({
-					delay: 500,
-					hideOnClick: true,
-					hideOnSwipe: true,
-					resetScroll: true,
-					resetForms: true,
-					side: 'left',
-					target: $body,
-					visibleClass: 'navPanel-visible'
-				});
-
+  // Panel.
+  $('<div id="navPanel">' + "<nav>" + $("#nav").navList() + "</nav>" + "</div>")
+    .appendTo($body)
+    .panel({
+      delay: 500,
+      hideOnClick: true,
+      hideOnSwipe: true,
+      resetScroll: true,
+      resetForms: true,
+      side: "left",
+      target: $body,
+      visibleClass: "navPanel-visible",
+    });
 })(jQuery);
+
+function openCity(evt, cityName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
